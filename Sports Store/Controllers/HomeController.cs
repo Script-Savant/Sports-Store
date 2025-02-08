4d7e312ba5b11d44129d436dd65d1ab8159f14f2
Alex Kinuthia <njugunakinuthia013@gmail.com>
2025-02-08 23:53:05 +0300

    filter by category first part

--- Sports Store/Controllers/HomeController.cs
+++ Sports Store/Controllers/HomeController.cs
@@ -15,18 +15,24 @@ namespace Sports_Store.Controllers
             _interface = inter;
         }
 
-        public IActionResult Index(int page=1)
+        public IActionResult Index(string? category, int page=1)
         {
             int pageSize = 4;
             var totalItems = _interface.Products.Count();
             var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
 
             var products = _interface.Products
+                .Where(p => category == null || p.Category == category)
                 .Skip((page-1) * pageSize)
                 .Take(pageSize);
 
+            var categories = _interface.Products
+                .Select(p => p.Category)
+                .Distinct();
+
             ViewBag.CurrentPage = page;
             ViewBag.TotalPages = totalPages;
+            ViewBag.Categories = categories;
 
             if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
                 return PartialView("_ProductListPartial", products);
